fd6cb507848b4908a08908d236462297dc7aa504
Dzmitry Samasiuk <dzmitry_samasiuk@epam.com>
2022-04-16 16:42:50 +0300

    Login repairs for non static methods

---
+++ src/test/java/login/LoginViaTrelloTest.java
@@ -0,0 +1,53 @@
+package login;
+
+import base.BaseTest;
+import commons.CommonActions;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.login.LoginViaTrelloPage;
+
+import static commons.CommonActions.driver;
+
+public class LoginViaTrelloTest extends BaseTest {
+
+    //TC ID TRE003 Incorrect credentials
+
+    @Test
+    public void loginWithinIncorrectCredentials() {
+        /*** Open https://trello.com/login. Type [Incorrect login credential example one] as a login. stay the password clear. **/
+        driver.manage().deleteAllCookies();
+        LoginViaTrelloPage loginViaTrelloPage = PageFactory.initElements(driver, LoginViaTrelloPage.class);
+        loginViaTrelloPage.loginWithNotRegisteredCredentials();
+    }
+
+    //TC ID TRE003 Incorrect credentials
+
+    @Test
+    public void loginWithinNotComfirmedEmail() throws InterruptedException {
+        /*** Type [Incorrect login credential example two] as a login. stay the password clear. **/
+        driver.manage().deleteAllCookies();
+        LoginViaTrelloPage loginViaTrelloPage = PageFactory.initElements(driver, LoginViaTrelloPage.class);
+        loginViaTrelloPage.loginWithNotConfirmedEmail();
+    }
+
+    //TC ID TRE003 Incorrect credentials
+    @Test
+    public void loginWithinEmptyPasswordField() {
+        /*** Type a [Login credential], stay the password clear. **/
+        driver.manage().deleteAllCookies();
+        LoginViaTrelloPage loginViaTrelloPage = PageFactory.initElements(driver, LoginViaTrelloPage.class);
+        loginViaTrelloPage.loginViaEmptyPassword();
+    }
+
+    //TC ID TRE004 Correct credentials
+
+    @Test
+    public void loginWithinCorrectCredentialsTest() throws InterruptedException {
+        /*** Type a [Login credential] and a [Password credential]. **/
+        /*** Access granted. The Boards page is opened. The Username is according to [Login credential] **/
+        LoginViaTrelloPage loginViaTrelloPage = PageFactory.initElements(driver, LoginViaTrelloPage.class);
+        //login into system is a @BeforeTest method
+        loginViaTrelloPage.checkTheUserExist();
+    }
+}
