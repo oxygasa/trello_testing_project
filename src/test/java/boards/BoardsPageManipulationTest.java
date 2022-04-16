ab62cb34e07a302c9fad261af04075e0400e0840
Dzmitry Samasiuk <dzmitry_samasiuk@epam.com>
2022-04-16 19:55:11 +0300

    Workspace, BoardPage tests are completed. Static cleanup.

--- src/test/java/boards/BoardsPageManipulationTest.java
+++ src/test/java/boards/BoardsPageManipulationTest.java
@@ -15,7 +15,15 @@ public class BoardsPageManipulationTest extends BaseTest {
 
     //TC ID TRE020 Boards page: Recently viewed.
     @Test
-    public void recentlyViewedTest() throws InterruptedException {
+    public void recentlyViewedBoardIsDisplayingOnBoardsPageTest() throws InterruptedException {
+        ///TC TRE021 Randomly catch recent viewed board in case of selenium reaction.
+        // List collection change size when recent board is cached and test this case.
+        boardsDisplayingInYourWorkspacesSectionTest();
+    }
+
+    //TC ID TRE021 Boards page: Your workspaces.
+    @Test
+    public void boardsDisplayingInYourWorkspacesSectionTest() throws InterruptedException {
         /*** Precondition: login, close all visible boards. Create 4 boards (and remember their names). **/
         BoardsPage boardsPage = PageFactory.initElements(driver, BoardsPage.class);
         CommonActions.closeAllVisibleBoards(boardsPage.getDefaultWorkspaceUrl());
@@ -24,23 +32,6 @@ public class BoardsPageManipulationTest extends BaseTest {
         System.out.println("Expected List" + expectedBoardNamesListener);
         /*** Check, the /boards page contains the same boards.**/
         List<String> actualResultBoardNames = boardsPage.saveBoardPageRecentBoardListToCollection();
-        System.out.println("Actual result " + actualResultBoardNames);
-        Assert.assertEquals(actualResultBoardNames, expectedBoardNamesListener);
-        /*** Post condition: close all visible boards **/
-        CommonActions.closeAllVisibleBoards(boardsPage.getDefaultWorkspaceUrl());
-    }
-
-    //TC ID TRE021 Boards page: Your workspaces.
-    @Test
-    public void yourWorkspacesTest() throws InterruptedException {
-        /*** Precondition: login, close all visible boards. Create 4 boards (and remember their names). **/
-        BoardsPage boardsPage = PageFactory.initElements(driver, BoardsPage.class);
-        CommonActions.closeAllVisibleBoards(boardsPage.getDefaultWorkspaceUrl());
-        List<String> expectedBoardNamesListener = boardsPage.createCollectionOfFiveExpectedBoards();
-        expectedBoardNamesListener.sort(String.CASE_INSENSITIVE_ORDER);
-        System.out.println("Expected List" + expectedBoardNamesListener);
-        /*** Check, the /boards page contains the same boards.**/
-        List<String> actualResultBoardNames = boardsPage.saveBoardPageWorkspaceBoardListToCollection();
         System.out.println("Actual result " + actualResultBoardNames);
         Assert.assertEquals(actualResultBoardNames, expectedBoardNamesListener);
         /*** Post condition: close all visible boards **/
